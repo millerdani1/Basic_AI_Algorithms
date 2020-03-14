6ce5785585b47fb9a7125c2a81b0d8ad153f0f36
millerdani1 <daniel.miller.djm@gmail.com>
2020-03-14 15:23:32 -0700

    replaced distance storage with PointDistance objects and replaced sorting method

--- COMP420_ASSIGN1/src/KNNAlgorithm.java
+++ COMP420_ASSIGN1/src/KNNAlgorithm.java
@@ -6,39 +6,25 @@ public class KNNAlgorithm {
 
         //for all points in the test data
         for(int i = 0; i<1; i++){
-            List<Map<Double, Integer>> list = new ArrayList<Map<Double, Integer>>();
-            //find distance to each training point and store in sorted list
-            for(int j = 0; j<training.size();j++){
-                Map<Double, Integer> map = new HashMap<Double, Integer>();
-                map.put(calculateEuclidianDistance(test.get(i).getAttributes(), training.get(j).getAttributes()), training.get(j).getWineClass());
-                list.add(map);
-            }
-            //choose first k points
 
+            //find distance to each training point and store in sorted list
+            List<PointDistance> distanceList = new ArrayList<PointDistance>();
+            for(int j = 0; j<training.size();j++){
+                distanceList.add(new PointDistance(calculateEuclidianDistance(test.get(i).getAttributes(), training.get(j).getAttributes()), training.get(j).getWineClass()));
+            }
+            Collections.sort(distanceList);
+
+            //choose first k points
+            ArrayList<Integer> classFrequency = new ArrayList<Integer>();
+            for(int q = 0; q <kValue; q++){
+                classFrequency.add(distanceList.get(q).getWineClass());
+            }
             //assign class to
 
-            Collections.sort(list, new Comparator<Map<Double, Integer>>() {
-                public int compare(Map<Double, Integer> o1, Map<Double, Integer> o2) {
-                    Collection<Double> values1 = o1.keySet();
-                    Collection<Double> values2 = o2.keySet();
-                    if(!values1.isEmpty() && !values2.isEmpty()){
-                        return values1.iterator().next().compareTo(values2.iterator().next());
-                    }else{
-                        return 0;
-                    }
-                }
-            });
 
 
 
 
-
-
-
-
-            for(int h =0;h<list.size();h++){
-                System.out.println(list.get(h));
-            }
         }
 
 
@@ -55,39 +41,6 @@ public class KNNAlgorithm {
             total = total + Math.pow((point1[i]-point2[i]), 2.0);
         }
         return Math.sqrt(total);
-    }
-
-
-
-
-
-
-
-
-    public static void printListContents(ArrayList<WineTestObject> training, ArrayList<WineTestObject> test){
-
-        //test to make sure everything is here
-        System.out.print("There are " + training.size() + " entries in the training list\n");
-        for(int i = 0;i<training.size();i++){
-            for(int j = 0;j<training.get(i).getAttributes().length;j++){
-                System.out.print(training.get(i).getAttributes()[j]);
-                System.out.print(" ");
-            }
-            System.out.print(training.get(i).getWineClass());
-            System.out.println("");
-        }
-
-        System.out.println("-----------------------------------------------------------");
-
-        System.out.print("There are " + test.size() + " entries in the test list\n");
-        for(int i = 0;i<test.size();i++){
-            for(int j = 0;j<test.get(i).getAttributes().length;j++){
-                System.out.print(test.get(i).getAttributes()[j]);
-                System.out.print(" ");
-            }
-            System.out.print(test.get(i).getWineClass());
-            System.out.println("");
-        }
     }
 
 
---
+++ COMP420_ASSIGN1/src/PointDistance.java
@@ -0,0 +1,31 @@
+import java.util.Comparator;
+
+public class PointDistance implements Comparator<PointDistance>, Comparable<PointDistance> {
+    private double distance;
+    private int wineClass;
+
+    public PointDistance(double distance, int wineClass){
+        this.distance=distance;
+        this.wineClass=wineClass;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public int getWineClass() {
+        return wineClass;
+    }
+
+    @Override
+    public int compareTo(PointDistance o) {
+        if(o.distance>this.distance) return -1;
+        else if(o.distance<this.distance) return 1;
+        else return 0;
+    }
+
+    @Override
+    public int compare(PointDistance o1, PointDistance o2) {
+        return (int) (o1.distance-o2.distance);
+    }
+}
