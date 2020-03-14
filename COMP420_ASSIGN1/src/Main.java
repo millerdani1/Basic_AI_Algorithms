2f89eaa7ea1fb254a374532ed69cddc7c294358d
millerdani1 <daniel.miller.djm@gmail.com>
2020-03-13 18:38:19 -0700

    file reader now returns arrayList of wineInstance objects

--- COMP420_ASSIGN1/src/Main.java
+++ COMP420_ASSIGN1/src/Main.java
@@ -1,13 +1,20 @@
+import java.util.ArrayList;
 
 public class Main {
 
     WineDataLoader dataLoader = new WineDataLoader();
+
+    ArrayList<WineInstance> wineTrainingData;
+    ArrayList<WineInstance> wineTestData;
 
     public static void main(String args[]){
         Main m = new Main(args);
     }
 
     public Main(String args[]){
+        wineTrainingData = new ArrayList<WineInstance>();
+        wineTestData = new ArrayList<WineInstance>();
+        
         if(args.length > 0) {
 
 
--- COMP420_ASSIGN1/src/WineDataLoader.java
+++ COMP420_ASSIGN1/src/WineDataLoader.java
@@ -1,29 +1,47 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.ArrayList;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class WineDataLoader {
 
-
-
-    public void readWineData(String filePath) {
+    //takes wine data file and returns ArrayList of WineInstance objects
+    public ArrayList readWineData(String filePath) {
+        ArrayList<WineInstance> arr = new ArrayList<WineInstance>();
         try {
             File wineFile = new File(filePath);
             Scanner scanner = new Scanner(wineFile);
             scanner.nextLine();//skips first line
+
             while (scanner.hasNextLine()) {
                 String data = scanner.nextLine();
-                //System.out.println(data);
-                generateWineInstance(data);
+                arr.add(generateWineInstance(data));
             }
             scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        return arr;
     }
 
-    private void generateWineInstance(String data){
+    //converts string of wine data and converts it into WineInstance object
+    private WineInstance generateWineInstance(String data){
 
+        String[] split = data.split("\\s+");
+
+        if(split.length!=14){
+            System.out.println("bad line found in file");
+            return null;
+        }
+
+        return new WineInstance(
+                Double.parseDouble(split[0]), Double.parseDouble(split[1]),
+                Double.parseDouble(split[2]), Double.parseDouble(split[3]),
+                Double.parseDouble(split[4]), Double.parseDouble(split[5]),
+                Double.parseDouble(split[6]), Double.parseDouble(split[7]),
+                Double.parseDouble(split[8]), Double.parseDouble(split[9]),
+                Double.parseDouble(split[10]), Double.parseDouble(split[11]),
+                Double.parseDouble(split[12]), Integer.parseInt(split[13]));
     }
 }
