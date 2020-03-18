5e6bb7275d6f1630257becf703435d4ebede243a
Daniel Miller <millerdani1@ecs.vuw.ac.nz>
2020-03-18 13:13:48 +1300

    program now takes k value as input on command line

--- COMP420_ASSIGN1/src/Main.java
+++ COMP420_ASSIGN1/src/Main.java
@@ -8,11 +8,24 @@ public class Main {
 
     public Main(String args[]){
 
-        if(args.length == 2) {
-            KNNAlgorithm.runAlgorithm(WineDataLoader.readWineData(args[0]), WineDataLoader.readWineData(args[1]), 2);
+        int kValue =1;
+        if(args.length == 3){
 
+            //ensure input k value is valid int
+            try{
+                kValue = Integer.parseInt(args[2].trim());
+            }
+            catch (NumberFormatException e){
+                System.out.println("K value must be valid int");
+                System.exit(0);
+
+            }
+        }
+
+        if(args.length == 2 || args.length == 3) {
+            KNNAlgorithm.runAlgorithm(WineDataLoader.readWineData(args[0]), WineDataLoader.readWineData(args[1]), kValue);
         } else {
-            System.out.println("Two input files are required to run this program");
+            System.out.println("This program requires two file names, and optionally a K value");
         }
 
     }
